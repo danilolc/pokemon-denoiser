51f46a446632fd791f80948c78e1a0c6db034e50
Danilo <dnlmcrd@gmail.com>
2024-06-03 21:12:43 -0300

    Good difusion run

--- UNet.py
+++ UNet.py
@@ -67,11 +67,15 @@ class NonLocalBlock(nn.Module):
         self.norm = GroupNorm(in_channels)
 
         # query, key and value layers
-        self.q = nn.Conv2d(in_channels, in_channels, 1, 1, 0)
-        self.k = nn.Conv2d(in_channels, in_channels, 1, 1, 0)
-        self.v = nn.Conv2d(in_channels, in_channels, 1, 1, 0)
+        self.q = nn.Conv2d(in_channels, in_channels, 1)
+        self.k = nn.Conv2d(in_channels, in_channels, 1)
+        self.v = nn.Conv2d(in_channels, in_channels, 1)
 
-        self.project_out = nn.Conv2d(in_channels, in_channels, 1, 1, 0)
+        self.project_out = nn.Sequential(
+            nn.Conv2d(in_channels, 4*in_channels, 1),
+            nn.GELU(),
+            nn.Conv2d(4*in_channels, in_channels, 1),
+        )
 
         self.softmax = nn.Softmax(dim=2)
 
@@ -121,13 +125,14 @@ class DoubleConv(nn.Module):
             nn.GELU(),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.GroupNorm(1, out_channels),
+            nn.GELU(),
         )
 
     def forward(self, x):
         if self.residual:
-            return F.gelu(x + self.double_conv(x))
+            return x + self.double_conv(x)
         else:
-            return F.gelu(self.double_conv(x))
+            return self.double_conv(x)
 
 class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -139,7 +144,7 @@ class Down(nn.Module):
             nn.GELU(),
         )
         self.conv = nn.Sequential(
-            DoubleConv(out_channels, out_channels, residual=True),
+            DoubleConv(out_channels, out_channels),
             DoubleConv(out_channels, out_channels, residual=True),
         )
 
@@ -154,8 +159,8 @@ class Up(nn.Module):
     def __init__(self, in_channels, skip_channels, out_channels):
         super().__init__()
         self.conv = nn.Sequential(
-            DoubleConv(in_channels + skip_channels, in_channels + skip_channels, residual=True),
             DoubleConv(in_channels + skip_channels, out_channels),
+            DoubleConv(out_channels, out_channels, residual=True),
         )
 
     def forward(self, x, skip_x):
@@ -168,40 +173,20 @@ class Up(nn.Module):
 class ContourEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.step1 = nn.Sequential(
-            DoubleConv(1, 32),
-            nn.MaxPool2d(2),
-            NonLocalBlock(32),
-        )
-        self.step2 = nn.Sequential(
-            DoubleConv(32, 64),
-            nn.MaxPool2d(2),
-            NonLocalBlock(64),
-        )
-        self.step3 = nn.Sequential(
-            DoubleConv(64, 128),
-            nn.MaxPool2d(2),
-            NonLocalBlock(128),
-        )
+        self.encoders = nn.ModuleList()
+        for i in [64, 128, 256, 256, 128, 64]:
+            self.encoders.append(nn.Linear(72 * 72, i))
 
     def forward(self, c):
-        c1 = self.step1(c)
-        c2 = self.step2(c1)
-        c3 = self.step3(c2)
-
-        return c1, c2, c3
+        return [enc(c)[:, :, None, None] for enc in self.encoders]
 
 class TimeEncoder(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
         self.emb_dim = emb_dim
         self.encoders = nn.ModuleList()
-        for i in [32, 64, 128, 64, 32]:
-            self.encoders.append(nn.Sequential(
-                #nn.Linear(emb_dim, emb_dim),
-                nn.SiLU(),
-                nn.Linear(emb_dim, i),
-            ))
+        for i in [64, 128, 256, 256, 128, 64]:
+            self.encoders.append(nn.Linear(emb_dim, i))
 
     def forward(self, t):
         t = pos_encoding(t, self.emb_dim)
@@ -211,83 +196,69 @@ class TimeEncoder(nn.Module):
 class UNet(nn.Module):
     def __init__(self, c_in=3, c_out=3, num_classes=10, time_dim=256):
         super().__init__()
-        self.label_emb = nn.Embedding(18, num_classes)
+        #self.label_emb = nn.Embedding(18, num_classes)
 
         self.enc = nn.Sequential(
-            nn.Conv2d(c_in, 16, kernel_size=1),
-            nn.GroupNorm(1, 16),
+            nn.Conv2d(c_in, 32, kernel_size=1),
+            nn.GroupNorm(1, 32),
             nn.GELU(),
             
-            DoubleConv(16, 16, residual=True),
-            DoubleConv(16, 16, residual=True),
-            #DoubleConv(16, 16, residual=True),
+            DoubleConv(32, 32, residual=True),
+            DoubleConv(32, 32, residual=True),
         )
         
-        self.down1 = Down(16, 32)
-        self.down2 = Down(32, 64)
-        self.down3 = Down(64, 128)
+        self.down1 = Down(32, 64)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
 
         self.bot = nn.Sequential(
-            DoubleConv(128, 128),
-            DoubleConv(128, 128),
+            DoubleConv(256, 256),
+            DoubleConv(256, 256),
         )
 
-        self.up1 = Up(128, 64, 64)
-        self.up2 = Up(64, 32, 32)
-        self.up3 = Up(32, 16, 16)
+        self.up1 = Up(256, 128, 128)
+        self.up2 = Up(128, 64, 64)
+        self.up3 = Up(64, 32, 32)
         
         self.dec = nn.Sequential(
-            DoubleConv(16, 16, residual=True),
-            DoubleConv(16, 16, residual=True),
-            #DoubleConv(16, 16, residual=True),
-            nn.Conv2d(16, c_out, kernel_size=1),
+            DoubleConv(32, 32, residual=True),
+            DoubleConv(32, 32, residual=True),
+            nn.Conv2d(32, c_out, kernel_size=1),
         )
 
         self.c_encoder = ContourEncoder()
         self.t_encoder = TimeEncoder(time_dim)
 
-        self.att1c = NonLocalBlock(32)
-        self.att1x = NonLocalBlock(32)
-
-        self.att2c = NonLocalBlock(64)
-        self.att2x = NonLocalBlock(64)
-        
-        self.att3c = NonLocalBlock(128)
-        self.att3x = NonLocalBlock(128)
-
-        self.att4c = NonLocalBlock(64)
-        self.att4x = NonLocalBlock(64)
-
-        self.att5c = NonLocalBlock(32)
-        self.att5x = NonLocalBlock(32)
+        self.att1 = NonLocalBlock(64)
+        self.att2 = NonLocalBlock(128)
+        self.att3 = NonLocalBlock(256)
+        self.att4 = NonLocalBlock(256)
+        self.att5 = NonLocalBlock(128)
+        self.att6 = NonLocalBlock(64)
 
     def forward(self, x, t, ty, c):
-        c1, c2, c3 = self.c_encoder(c)
-        t1, t2, t3, t4, t5 = self.t_encoder(t)
+        #c1, c2, c3 = self.c_encoder(c.flatten(1,3))
+        t1, t2, t3, t4, t5, t6 = self.t_encoder(t)
         
         x1 = self.enc(x)
         
         x2 = self.down1(x1)
-        x2 = self.att1c(x2 + c1 + t1)
-        #x2 = self.att1x(x2 + t1)
+        x2 = self.att1(x2 + t1)
 
         x3 = self.down2(x2)
-        x3 = self.att2c(x3 + c2 + t2)
-        #x3 = self.att2x(x3 + t2)
+        x3 = self.att2(x3 + t2)
 
         x4 = self.down3(x3)
-        x4 = self.att3c(x4 + c3 + t3)
-        #x4 = self.att3x(x4 + t3)
+        x4 = self.att3(x4 + t3)
 
         x4 = self.bot(x4)
+        x4 = self.att4(x4 + t4)
 
         x4 = self.up1(x4, x3)
-        x4 = self.att4c(x4 + c2 + t4)
-        #x4 = self.att4x(x4 + t4)
+        x4 = self.att5(x4 + t5)
 
         x4 = self.up2(x4, x2)
-        x4 = self.att5c(x4 + c1 + t5)
-        #x4 = self.att5x(x4 + t5)
+        x4 = self.att6(x4 + t6)
 
         x4 = self.up3(x4, x1)
 
